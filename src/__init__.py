e0b38345f521d112e153d7a21fc18c519631a96e
WeiTing1991 <>
2025-08-30 15:35:41 +0200

    prepare for release with 0.1.0

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Control backend package for digital casting system"""
-
-__author__ = ["Wei-Ting Chen"]
-__copyright__ = "2024 FFMA"
-__license__ = "MIT License"
-__email__ = "chenwei@arch.ethz.ch"
-__version__ = "0.1.0"
